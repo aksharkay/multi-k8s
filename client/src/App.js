ba488475c429db1cdc3fa9c7c55350f9e7f47128
aksharkay <aksharkomm@gmail.com>
2023-12-14 13:34:21 +0530

    changed title text

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to React!
+            Fib Calculator
           </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
