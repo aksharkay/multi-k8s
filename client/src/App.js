fd34dee179e97719e2c9a255577d80570ee2b48a
aksharkay <aksharkomm@gmail.com>
2023-12-16 11:28:18 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Fib Calculator
+            Fib Calculator - v2
           </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
